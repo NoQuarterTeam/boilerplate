f58e233b454fc4d34bf048f1b6dfbb16998f69ff
Jack Clackett <jack@noquarter.co>
2023-04-14 17:09:44 +0200

    fix expo init location

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,13 +1,14 @@
+process.env.EXPO_ROUTER_APP_ROOT = __dirname + "/src/app"
+
 module.exports = function (api) {
   api.cache(true)
-  process.env.EXPO_ROUTER_APP_ROOT = "../../apps/app/src/app"
   return {
     presets: [["module:metro-react-native-babel-preset", { useTransformReactJSXExperimental: true }], "babel-preset-expo"],
     plugins: [
+      require.resolve("expo-router/babel"),
       ["@babel/plugin-transform-react-jsx", { runtime: "automatic" }],
       "nativewind/babel",
       "react-native-reanimated/plugin",
-      require.resolve("expo-router/babel"),
     ],
   }
 }
