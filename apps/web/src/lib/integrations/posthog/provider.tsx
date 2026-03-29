4b4ecaac23f8994a4b6c7b51a0c53cf35c581483
Jack Clackett <jack@noquarter.co>
2026-03-29 11:55:05 +0200

    ph

--- apps/web/src/lib/integrations/posthog/provider.tsx
+++ apps/web/src/lib/integrations/posthog/provider.tsx
@@ -6,8 +6,7 @@ if (typeof window !== "undefined" && import.meta.env.VITE_POSTHOG_KEY) {
   posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
     api_host: import.meta.env.VITE_POSTHOG_HOST || "https://eu.i.posthog.com",
     person_profiles: "identified_only",
-    capture_pageview: false,
-    defaults: "2025-11-30",
+    defaults: "2026-01-30",
   })
 }
 
