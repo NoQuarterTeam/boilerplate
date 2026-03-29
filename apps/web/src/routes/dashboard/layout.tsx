5145079165ca3e31af1fe1021998d8c3fb90b793
Jack Clackett <jack@noquarter.co>
2026-03-29 18:12:41 +0200

    Refactor dashboard layout by commenting out SidebarProvider and related components, while retaining the structure for improved code clarity and maintainability.

--- apps/web/src/routes/dashboard/layout.tsx
+++ apps/web/src/routes/dashboard/layout.tsx
@@ -1,10 +1,13 @@
 import { queryOptions } from "@tanstack/react-query"
 import { createFileRoute, Outlet } from "@tanstack/react-router"
 
+import { Separator } from "@boilerplate/ui/components/separator"
 import { TooltipProvider } from "@boilerplate/ui/components/tooltip"
 
 import { DefaultError } from "@/components/default-error"
 
+import { DashboardBreadcrumbs } from "./-components/dashboard-breadcrumbs"
+import { NavUser } from "./-components/nav-user"
 // import { NavUser } from "./-components/nav-user"
 import { getCurrentUserFn } from "./-data"
 
@@ -27,24 +30,26 @@ export const Route = createFileRoute("/dashboard")({
 function DashboardLayoutShell() {
   return (
     <TooltipProvider delay={0}>
-      {/* <SidebarProvider>
-        <div className="flex h-dvh w-full overflow-hidden">
-          <DashboardSidebar />
-          <SidebarInset className="flex flex-col overflow-hidden">
-            <header className="z-30 flex h-16 w-full shrink-0 items-center justify-between gap-2 border-b bg-background px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-              <div className="flex items-center gap-2">
-                <SidebarTrigger className="-ml-1" />
-                <Separator orientation="vertical" className="mt-2 mr-2 data-[orientation=vertical]:h-4" />
-                <DashboardBreadcrumbs />
-              </div>
-              <div className="flex shrink-0 items-center gap-2"><NavUser /></div>
-            </header>
-            <div className="flex-1 overflow-auto overscroll-contain"> */}
-      <Outlet />
-      {/* </div>
-          </SidebarInset>
+      {/* <SidebarProvider> */}
+      <div className="flex h-dvh w-full overflow-hidden">
+        {/* <DashboardSidebar /> */}
+        {/* <SidebarInset className="flex flex-col overflow-hidden"> */}
+        <header className="z-30 flex h-16 w-full shrink-0 items-center justify-between gap-2 border-b bg-background px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2">
+            {/* <SidebarTrigger className="-ml-1" /> */}
+            <Separator orientation="vertical" className="mt-2 mr-2 data-[orientation=vertical]:h-4" />
+            <DashboardBreadcrumbs />
+          </div>
+          <div className="flex shrink-0 items-center gap-2">
+            <NavUser />
+          </div>
+        </header>
+        <div className="flex-1 overflow-auto overscroll-contain">
+          <Outlet />
         </div>
-      </SidebarProvider> */}
+        {/* </SidebarInset> */}
+      </div>
+      {/* </SidebarProvider> */}
     </TooltipProvider>
   )
 }
